75870b99df257c56b8bbd733a2ff948cb6b05a1b
sukanyawebdev <sukanyat211@gmail.com>
2024-04-07 21:34:48 +0530

    solution 2

--- src/components/ProductGrid/index.js
+++ src/components/ProductGrid/index.js
@@ -17,7 +17,7 @@ class ProductGrid extends Component {
 
   fetchProducts = async () => {
     try {
-      const response = await fetch('https://fakestoreapi.com/products');
+      const response = await fetch('https://appscrip-task-backend-2.onrender.com/products');
       const data = await response.json();
       this.setState({ products: data });
     } catch (error) {
